4e8abf1ac675c75104c16538d19c83e627c9ccf7
GenBor <Generalovboris@outlook.com>
2018-05-08 16:09:05 +0300

    var2

--- resto.py
+++ resto.py
@@ -1,52 +1,47 @@
-import shelve
-#https://code.activestate.com/recipes/576642/
-#https://pymotw.com/2/shelve/
-#https://pythoner.name/shelve-objects
-#https://www.quora.com/How-do-I-put-dictionary-to-the-shelve-in-Python
+#import shelve
+
+
+import exeptions as ex
 class Drink:
 
-    def __init__(self,name = None,amount = None,**data):
+    def __init__(self,data=None):
         self._data = data
-        self.name = name
-        self.amount = amount
-    def get(self,key): #метод для возврата значения
-        return self._data[key]
-    def set(self,key,value)->None: #метод присваивания нового значения
-        self._data[key] = value
+    def get(self,drinks): #метод для возврата значения
+        return self._data[drinks]
+    def set(self,drinks,reserves)->None: #метод присваивания нового значения
+        self._data[drinks] = reserves
     def add(self,x):
         self._data.update(x)
     def _dict(self)->dict:
         return self._data
     def __str__(self)->str:
-        return (";\n".join("%s=>%s" % i for i in self._data.items()))
+        return (";\n".join("%s=>%s" % drinks for drinks in self._data.items()))
 
 ################################################################################
-d = shelve.open("db_file")  # open -- file may get suffix added by low-level
-print(d)
-"""                           # library
-data = {"name":"Vodka","amount":4.5}
-d[key] = data              # store data at key (overwrites old data if
-                           # using an existing key)
-data = d[key]              # retrieve a COPY of data at key (raise KeyError
-                           # if no such key)
-del d[key]                 # delete data stored at key (raises KeyError
-                           # if no such key)
+class Drinks(Drink):
+    def __init__(self,data):
+        super().__init__(data)
 
-flag = key in d            # true if the key exists
-klist = list(d.keys())     # a list of all existing keys (slow!)
+    def allex(self):
+        for k in self._data:
+            try:
+                if i in self._data:
+                    raise ex.AlExists()
+            except ex.AlExists() as Al:
+                print(Al)
 
-# as d was opened WITHOUT writeback=True, beware:
-d['xx'] = [0, 1, 2]        # this works as expected, but...
-d['xx'].append(3)          # *this doesn't!* -- d['xx'] is STILL [0, 1, 2]!
+    def not_found(self):
+        for i in self._data:
+            try:
+                if i not in self._data.keys():
+                    raise ex.NotFound()
+            except ex.NotFound() as nt:
+                print(nt)
 
-# having opened d without writeback=True, you need to code carefully:
-temp = d['xx']             # extracts the copy
-temp.append(5)             # mutates the copy
-d['xx'] = temp             # stores the copy right back, to persist it
-
-# or, d=shelve.open(filename,writeback=True) would let you just code
-# d['xx'].append(5) and have it work as expected, BUT it would also
-# consume more memory and make the d.close() operation slower.
-
-d.close()                  # close it
-"""
+    def invVol(self):
+        for i in self._data:
+            try:
+                if reserves >= 0.5:
+                    raise ex.InvalidVol()
+            except ex.InvalidVol() as inv:
+                print(inv)
--- test_resto.py
+++ test_resto.py
@@ -1,1 +1,19 @@
 #https://github.com/mafm/ledger.py/blob/master/test_ledger.py
+import resto
+
+################################################################################
+drinks ={"Name":"Pivo","amount":8}
+dr = resto.Drinks(drinks)
+print(dr)
+
+d = {"Name": "Vodka","amount":5}
+dr.add(d)
+print(dr)
+d = {"Name": "Vodka","amount":5}
+dr.add(d)
+print(dr)
+print(dr._dict())
+dr.set("Martini",15)
+print(dr)
+dr.set("Martini",15)
+print(dr)
