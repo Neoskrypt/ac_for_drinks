70655a2a8c90d64f014061bc8481d1acbf0ad36f
Aleksandr Sokolov <albartash@yahoo.com>
2018-05-17 22:02:19 +0300

    freeze

--- resto.py
+++ resto.py
@@ -18,48 +18,39 @@ class DictDrink:
 
     def __str__(self)->str:
         return "Drink {} ({} litres)".format(self.name,self.amount)
-class Adder(DictDrink):
+
+
+class Adder:
     def __init__(self,name,amount):
         super().__init__(name,amount)
-    def add(self):
-        db = shelve.open("db","c")
-        name = "Drink"
-        db[name] = DictDrink.to_dict(self)
-        if self.name in db:
-            raise ex.DrinkAlreadyExists("")
-        elif self.amount <= 0 or self.amount > 10:
-            raise ex.InvalidVol("")
-        elif self.name.isdigit() and self.amount.isalpha():
-            raise ex.PassArgs("")
-        elif self.name is "":
-            raise ex.PassArgs("")
-        else:
-            print("Drink successfully added!!!\n name = {} litres = {}".
-            format(self.name,self.amount))
-        return db[name]
-    def display(self,name):
-        """ вывод на экран обьектов заданного типа"""
-        db = shelve.open("db")
-        if self.name not in db:
-            """ если напитка нет в БД"""
-            raise ex.NotFound("")
-        else:
-            db = db.get(name,"Undefined")
-            #print(db) # print as dict
-            print((";\n".join("%s=>%s" % i for i in db.items())))
+
     @staticmethod
-    def _list():
+    def add(obj: DictDrink ):
+        db = shelve.open("db", "c")
+        if obj.name in db:
+            raise ex.DrinkAlreadyExists("")
+        elif obj.amount <= 0 or obj.amount > 10:
+            raise ex.InvalidVol("")
+        elif obj.name is "":
+            raise ex.PassArgs("")
+        else:
+            db[obj.name] = DictDrink.to_dict(obj)
+            print("Drink successfully added!!!\n name = {} litres = {}".
+            format(obj.name, obj.amount))
+
+    @staticmethod
+    def list(obj_type):
         """print list of drinks"""
         db = shelve.open("db", flag='c', protocol=None, writeback=False)
-        names_only = input("Names only [Y/n] ->")
 
-        if names_only == "Y":
+        if obj_type == 'drinks':
             for name in db.keys():
                 print(name)
-        elif names_only == "n":
-            for key in db.items():
-                print(key, sep=' ', end='\n', file=sys.stdout, flush=False)
-            #print((";\n".join("%s=>%s" % i for i in db.items())))
+        elif obj_type == 'reserves':
+            for key in db.keys():
+                print("{} ({} litres)".format(key, db[key]['amount']))
+        else:
+            raise ex.PassArgs("Incorrect object type: '{}'".format(obj_type))
 
 
 ###############################################################################
@@ -72,17 +63,15 @@ if __name__ == "__main__":
 
     if len(sys.argv) > 1:
         arg_command = sys.argv[1]
-        print("list", sep=' ', end='\n', file=sys.stdout, flush=False) # for to check
-        print("add", sep=' ', end='\n', file=sys.stdout, flush=False) # for to check
-        #command = input()
     else:
         sys.exit(1)
 
-    if arg_command == "add":
+    if arg_command == "add" and len(sys.argv) > 2:
 
-        b = DictDrink("Borjomi",1.5)
+        b = DictDrink(sys.argv[2], 10)
         c = Adder.add(b)
-    elif arg_command == "list":
-        c = Adder._list()
+    elif arg_command == "list" and len(sys.argv) > 2:
+        obj_type = sys.argv[2]
+        Adder.list(obj_type)
     else:
-        sys.exit(1)
+        raise ex.PassArgs("Incorrect arguments passed.")
